63d35f32af8f27cb2fbaa5513898a6916f86b66f
Subaru <davidqxzz123@gmail.com>
2023-01-16 10:51:13 +0800

    build(fastapi): SQLAlchemy ORM
    Build the "User" table object for alembic auto migration
    
    Ref: https://fastapi.tiangolo.com/tutorial/sql-databases/

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# postgresql://user:password@postgresserver/db
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres123@0.0.0.0:5432/userinfo_db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Date, DateTime
+from datetime import datetime
+
+from database import Base
+
+class User(Base):
+    __tablename__ = "userinfo_tb"
+
+    name = Column(String, primary_key=True)
+    passwd = Column(String, nullable=False)
+    birthday = Column(Date)
+    createT = Column(DateTime, default = datetime.utcnow())
+    last_login = Column(DateTime, nullable=True)
