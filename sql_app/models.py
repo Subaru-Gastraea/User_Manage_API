33a9bacee0d99abea020ca8da2e7c6a823da9c03
Subaru <davidqxzz123@gmail.com>
2023-01-30 13:07:15 +0800

    fix(sql_app): Fix relative import problem
    Move files of FastAPI to new folder "sql_app"

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Date, DateTime
 from datetime import datetime
 
-from database import Base
+from .database import Base
 
 class User(Base):
     __tablename__ = "userinfo_tb"
