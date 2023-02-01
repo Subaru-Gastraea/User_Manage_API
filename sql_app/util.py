a3d378d177b704e7c16a23412db494d4c623f1f1
Subaru <davidqxzz123@gmail.com>
2023-02-01 18:26:28 +0800

    feat(util.py): Functions for handling password hashes and JWT authorization
    Hashing algorithm: sha256

---
+++ sql_app/util.py
@@ -0,0 +1,34 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+
+pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "189a8b31ac529bad505f9c3fa552c2bfc569f2a4512bf9ff71985e1172560c4a"
+ALGORITHM = "HS256"
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def get_token_data(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    
+    return payload
