81d2b924ca40e4ee687096bc2f0392befa82058c
Subaru <davidqxzz123@gmail.com>
2023-02-01 18:19:59 +0800

    feat(crud.py): Add authentication function. Change password hashing method to the real one

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,16 +1,22 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, util
 from datetime import datetime
 
-def fake_hash_password(password: str):
-    return password + "notreallyhashed"
+def authenticate_user(db: Session, username: str, password: str):
+    db_user = get_user(db, user_name=username)
+    if not db_user:
+        return False
+    if not util.verify_password(password, db_user.passwd):
+        return False
+
+    return db_user
 
 def get_user(db: Session, user_name: str):
     return db.query(models.User).filter(models.User.name == user_name).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = fake_hash_password(user.password)
-    db_user = models.User(name=user.name, birthday=user.birthday,  passwd=fake_hashed_password)
+    hashed_password = util.get_password_hash(user.password)
+    db_user = models.User(name=user.name, birthday=user.birthday,  passwd=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
