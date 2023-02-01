1661e1a8fd1e0fe2d86dc4748eb2f7463416c426
Subaru <davidqxzz123@gmail.com>
2023-02-01 09:52:08 +0800

    feat(main.py,-crud.py): Set up login endpoint
    Not yet completed (token value, JWT authorization, password hashing, etc.), just a simple version

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,14 +1,22 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
+from datetime import datetime
+
+def fake_hash_password(password: str):
+    return password + "notreallyhashed"
 
 def get_user(db: Session, user_name: str):
     return db.query(models.User).filter(models.User.name == user_name).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
+    fake_hashed_password = fake_hash_password(user.password)
     db_user = models.User(name=user.name, birthday=user.birthday,  passwd=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    
-    return db_user
+
+def user_login(db: Session, user_name: str):
+    db.query(models.User).filter(models.User.name == user_name).update(
+        {"last_login": datetime.utcnow()}, synchronize_session="fetch"
+    )
+    db.commit()
--- sql_app/main.py
+++ sql_app/main.py
@@ -1,11 +1,13 @@
 from fastapi import Depends, FastAPI, HTTPException, Form
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # Dependency
 def get_db():
@@ -16,7 +18,19 @@ def get_db():
         db.close()
 
 # Login
-# @app.post("/login")
+@app.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_name=form_data.username)
+    if db_user is None:
+        raise HTTPException(status_code=400, detail="User not found")
+
+    hashed_password = crud.fake_hash_password(form_data.password)
+    if not hashed_password == db_user.passwd:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    crud.user_login(db, user_name=db_user.name)
+
+    return {"access_token": db_user.name, "token_type": "bearer"}
 
 # Get user data
 @app.get("/user/", response_model=schemas.User, status_code=200)
