b1af3b76d5b9279e842343936ba55fe6f5d56e85
Subaru <davidqxzz123@gmail.com>
2023-02-02 09:57:17 +0800

    feat(main.py): Set up endpoint for deleting users

--- sql_app/main.py
+++ sql_app/main.py
@@ -43,7 +43,7 @@ async def Auth_current_user(token: str = Depends(oauth2_scheme), db: Session = D
 @app.post("/login", response_model=schemas.Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     # Authenticate
-    user = crud.authenticate_user(db, form_data.username, form_data.password)
+    user = crud.authenticate_user(db=db, username=form_data.username, password=form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -52,7 +52,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         )
 
     # Update time of last login
-    crud.user_login(db, user_name=user.name)
+    crud.user_login(db=db, user_name=user.name)
 
     # Generate access token for JWT authorization
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -65,7 +65,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 # Get user data
 @app.get("/user/", response_model=schemas.User, status_code=200)
 def read_user(name: str = Form(), cur_uname: str = Depends(Auth_current_user), db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_name=name)
+    db_user = crud.get_user(db=db, user_name=name)
     if db_user is None:
         raise HTTPException(status_code=400, detail="User not found")
         
@@ -74,14 +74,20 @@ def read_user(name: str = Form(), cur_uname: str = Depends(Auth_current_user), d
 # Create user
 @app.post("/user/", status_code=200)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_name=user.name)
+    db_user = crud.get_user(db=db, user_name=user.name)
     if db_user:
         raise HTTPException(status_code=400, detail="User name already registered")
     
     crud.create_user(db=db, user=user)
 
 # Delete user
-# @app.delete("/user/")
+@app.delete("/user/", status_code=200)
+def delete_user(name: str = Form(), cur_uname: str = Depends(Auth_current_user), db: Session = Depends(get_db)):
+    db_user = crud.get_user(db=db, user_name=name)
+    if db_user is None:
+        raise HTTPException(status_code=400, detail="User not found")
+
+    crud.delete_user(db=db, user_name=name)
 
 # Update password & birthday
 # @app.patch("/user/")
